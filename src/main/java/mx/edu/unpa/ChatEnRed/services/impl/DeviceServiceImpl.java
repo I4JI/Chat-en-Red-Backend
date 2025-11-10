f85f692a005cd17a50964a19295206cad8ad7f8f
I4JI <hernandezaguirrej204@gmail.com>
2025-11-09 18:17:46 -0600

    feat: crea DeviceServiceImpl.java

---
+++ src/main/java/mx/edu/unpa/ChatEnRed/services/impl/DeviceServiceImpl.java
@@ -0,0 +1,43 @@
+package mx.edu.unpa.ChatEnRed.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.edu.unpa.ChatEnRed.domains.Device;
+import mx.edu.unpa.ChatEnRed.repositories.DeviceRepository;
+import mx.edu.unpa.ChatEnRed.services.DeviceService;
+
+@Service
+public class DeviceServiceImpl implements DeviceService {
+
+    @Autowired
+    private DeviceRepository repo;
+
+    @Override
+    public Iterable<Device> findAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Optional<Device> findById(int id) {
+        return repo.findById(id);
+    }
+
+    @Override
+    public List<Device> findByUserId(int userId) {
+        return repo.findByUserIdOrderByCreatedAtDesc(userId);
+    }
+
+    @Override
+    public Device save(Device device) {
+        return repo.save(device);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
+}
