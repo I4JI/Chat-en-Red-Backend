39ac0e9cf55d8310f673573a4d65ac48bf60ed1e
Andres Gonzalez <manuel07andres@gmail.com>
2025-11-10 17:23:58 -0600

    fix(User)
    Se agregaron notaciones

--- src/main/java/mx/edu/unpa/ChatEnRed/domains/User.java
+++ src/main/java/mx/edu/unpa/ChatEnRed/domains/User.java
@@ -1,13 +1,21 @@
 package mx.edu.unpa.ChatEnRed.domains;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+
+@Data
+@Entity
+@Table(name="users")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
