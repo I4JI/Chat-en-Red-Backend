6e327121066bb619e83c3d3261645f4c1927f182
Valentino <valentinomatamoros2@gmail.com>
2025-11-11 21:33:49 -0600

    fix(userController)

--- src/main/java/mx/edu/unpa/ChatEnRed/controllers/UserController.java
+++ src/main/java/mx/edu/unpa/ChatEnRed/controllers/UserController.java
@@ -6,17 +6,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import mx.edu.unpa.ChatEnRed.domains.User;
 import mx.edu.unpa.ChatEnRed.services.UserService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
